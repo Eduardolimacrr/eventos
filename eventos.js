fba4571fa5b131124b87fd60599f251c1dc1f3d6
Eduardo Lima <eduardolimacrr@gmail.com>
2024-06-26 15:20:29 -0300

    eventos2

--- eventos.js
+++ eventos.js
@@ -1,4 +1,24 @@
-document.getElementById('teste').addEventListener('click', (event)  =>{
+function executarAcao(event){
     console.log('ola')
     console.log('event')
+}
+
+document.getElementById('form').addEventListener('subit', (event) => {
+    event.preventDefault();
+    console.log('event')
+    alert('Recebi o evento')
+})
+
+const btnEmail =  document.getElementById('testeEmail')
+
+btnEmail.addEventListener('focus', (event) => {
+    console.log('focou no elemento')
+})
+
+btnEmail.addEventListener('blur', (event) => {
+    console.log('desfocou do elemento')
+})
+
+window.addEventListener('resize', (event) => {
+    console.log("mudou o tamanho")
 })
